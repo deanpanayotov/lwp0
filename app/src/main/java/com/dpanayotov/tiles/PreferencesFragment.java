6aa08c35078b753157b46afb1473be8a6b2c1c6e
Dean Panayotov <dpanayotov@melontech.com>
2015-09-02 02:37:08 +0300

    Working preferences.

---
+++ app/src/main/java/com/dpanayotov/tiles/PreferencesFragment.java
@@ -0,0 +1,45 @@
+package com.dpanayotov.tiles;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.widget.Toast;
+
+/**
+ * Created by Dean Panayotov Local on 2.9.2015.
+ */
+public class PreferencesFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.prefs);
+
+        // add a validator to the "numberofCircles" preference so that it only
+        // accepts numbers
+        Preference circlePreference = getPreferenceScreen().findPreference("numberOfCircles");
+
+        // add the validator
+        circlePreference.setOnPreferenceChangeListener(numberCheckListener);
+    }
+
+    /**
+     * Checks that a preference is a valid numerical value
+     */
+
+    Preference.OnPreferenceChangeListener numberCheckListener = new Preference.OnPreferenceChangeListener() {
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            // check that the string is an integer
+            if (newValue != null && newValue.toString().length() > 0
+                    && newValue.toString().matches("\\d*")) {
+                return true;
+            }
+            // If now create a message to the user
+            if(getActivity() != null){
+                Toast.makeText(getActivity(), "Invalid Input", Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+    };
+}
--- app/src/main/java/com/dpanayotov/tiles/TilesActivity.java
+++ app/src/main/java/com/dpanayotov/tiles/TilesActivity.java
@@ -1,12 +1,16 @@
 package com.dpanayotov.tiles;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.app.WallpaperManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Created by Dean Panayotov Local on 2.9.2015 г..
@@ -24,6 +28,17 @@ public class TilesActivity extends Activity{
                 intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
                         new ComponentName(TilesActivity.this, TilesService.class));
                 startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.preferences).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment fragment = new PreferencesFragment();
+                fragmentTransaction.add(R.id.frame, fragment, "-");
+                fragmentTransaction.commit();
             }
         });
     }
